99229b78e50393c5741add5f870bfdffd9fe8921
paypaaji <paaji041@gmail.com>
2024-10-17 04:25:35 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<ilw-call-to-action>
+    <img src="https://cdn.brand.illinois.edu/icons/solid/white/comment.svg" alt="" slot="icon">
+    <h3>Training Opportunities</h3>
+    <p>Need more in-depth guidance on building Illinois websites? Check out these resources.</p>
+    <ul class="ilw-buttons">
+        <li><a href="#">Brand training resources</a></li>
+        <li><a href="#">Web components guides</a></li>
+    </ul>
+</ilw-call-to-action>
