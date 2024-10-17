af0b553666990db42b4980a48282ae618ef39a7b
paypaaji <paaji041@gmail.com>
2024-10-17 04:38:26 +0400

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<ilw-call-to-action>
-    <img src="https://cdn.brand.illinois.edu/icons/solid/white/comment.svg" alt="" slot="icon">
-    <h3>PAY.PAL CUSTOMER SERVICE</h3>
-    <p>Need more in-depth guidance on building Illinois websites? Check out these resources.</p>
-    <ul class="ilw-buttons">
-        <li><a href="#">Brand training resources</a></li>
-        <li><a href="#">Web components guides</a></li>
-    </ul>
-</ilw-call-to-action>
